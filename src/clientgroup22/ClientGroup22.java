8f3e7588581724425af4c24a73237cbebe95fd6d
Matthew <matthew@matthew-mint.(none)>
2013-04-08 12:21:22 +0200

    initial commit

---
+++ src/clientgroup22/ClientGroup22.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clientgroup22;
+
+/**
+ *
+ * @author matthew
+ */
+public class ClientGroup22 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("poop");
+    }
+}
