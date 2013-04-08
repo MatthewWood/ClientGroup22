ec8f20bd1299f5691ecf012665db024b4672400e
Wesley Robinson <rbnwes001@sl-dual-24.cs.uct.ac.za>
2013-04-08 12:33:09 +0200

    Test change

--- src/clientgroup22/ClientGroup22.java
+++ src/clientgroup22/ClientGroup22.java
@@ -14,6 +14,6 @@ public class ClientGroup22 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("poop");
+        System.out.println("poopfeast420");
     }
 }
