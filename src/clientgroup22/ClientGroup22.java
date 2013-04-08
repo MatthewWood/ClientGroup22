ccf35c13dcd9857b30702f39cb603bbe189c7287
matthew <matthew@matthew-mint>
2013-04-08 18:49:50 +0200

    Actual project start, simple connection code only

--- src/clientgroup22/ClientGroup22.java
+++ src/clientgroup22/ClientGroup22.java
@@ -4,15 +4,35 @@
  */
 package clientgroup22;
 
+import java.io.*;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 /**
  *
  * @author matthew
  */
 public class ClientGroup22 {
+
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        System.out.println("Good day sir");
+    public static void main(String[] args) throws UnknownHostException, IOException {
+
+        Connect();
+
+    }
+
+    public static void Connect() {
+        try {
+            Socket sock = new Socket("197.85.191.195", 3000);
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(sock.getInputStream()));
+            PrintWriter out = new PrintWriter(sock.getOutputStream(), true);
+
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+
     }
 }
