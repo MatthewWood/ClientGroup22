2c42f4493a23e59311e3f0a13a0d7ba2e16cf754
wesley <wesley@Wesley-pc>
2013-04-10 00:48:55 +0200

    Included query functionality, only returns JSONObject, no formatting yet!

--- src/clientgroup22/ClientGroup22.java
+++ src/clientgroup22/ClientGroup22.java
@@ -22,20 +22,22 @@ import java.util.Scanner;
  */
 public class ClientGroup22 {
 
-    ArrayList<String> data;
-
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) throws FileNotFoundException, IOException {
 
         Socket s = Connect();
+
         Ping(s);
-        GetDataSumm(s);
 
-        UploadData(s);
+//        GetDataSumm(s);
 
-        ClientGroup22 client = new ClientGroup22();
+//        UploadData(s);
+
+        QueryData(s);
+
+//        ClientGroup22 client = new ClientGroup22();
 
     }
 
@@ -71,13 +73,13 @@ public class ClientGroup22 {
         JSONObject reading;
         JSONArray readings = new JSONArray();
         String type = "";
-        double value = 0.0;
+        String value = "";
         long time = 0;
         for (int i = 0; i < dataArr.size(); i++) {
             reading = new JSONObject();
 
             type = dataArr.get(i).split(" ")[0];
-            value = Double.parseDouble(dataArr.get(i).split(" ")[1]);
+            value = dataArr.get(i).split(" ")[1];
             time = Long.parseLong(dataArr.get(i).split(" ")[2]);
 
             reading.put("type", type);
@@ -96,7 +98,7 @@ public class ClientGroup22 {
         finalSend.put("group_id", "22");
         finalSend.put("params", params);
         finalSend.put("method", "new_readings");
-        
+
 //        System.out.println(finalSend.toString());
 
         try {
@@ -106,10 +108,108 @@ public class ClientGroup22 {
             out.write(finalSend.toString());
             out.println();
 
+//            JSONObject returned = new JSONObject(in.readLine()); //Object to store returning string from server
+
+//            System.out.println("Reply from Server:" + returned.getString("result"));
+            System.out.println("Reply from Server:" + in.readLine());
+
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+    }
+
+    public static void QueryData(Socket s) {
+        JSONObject query = new JSONObject();
+        JSONObject params = new JSONObject();
+        JSONObject returned = new JSONObject();
+//        JSONObject time_from = new JSONObject();  //Probably don't need...
+//        JSONObject time_to = new JSONObject();
+        JSONArray group_ids = new JSONArray();
+        JSONArray types = new JSONArray();
+
+        /*Group ids*/
+        System.out.println("Please enter in the groups to query by their group ids separated by a space (Enter to confirm):");
+//        String[] group_idArr = ((new Scanner(System.in)).nextLine()).split(" ");
+        String[] group_idArr = {"22"};
+        for (String i : group_idArr) {
+            if (isInteger(i)) {
+                group_ids.put(Integer.parseInt(i));
+            } else {
+                System.out.println(i + " is not an integer, ignoring.");
+            }
+        }
+        params.put("group_ids", group_ids);
+
+        /*Time from*/
+        System.out.println("Please enter the time from which you want readings (yyyy-mm-dd hh:mm:ss):");
+//        params.put("time_from", new Scanner(System.in).nextLine());
+        params.put("time_from", "2013-01-01 01:01:01");
+        
+        /*Time to*/
+        System.out.println("Please enter the time to which you want readings (yyyy-mm-dd hh:mm:ss):");
+//        params.put("time_to", new Scanner(System.in).nextLine());
+        params.put("time_to", "2013-04-09 23:55:01");
+
+        /*Types*/
+        System.out.println("Please enter which types of readings you want to query, separated by a space (\"light\", \"temperature\", \"humidity\")");
+//        String[] typesArr = ((new Scanner(System.in)).nextLine()).split(" ");
+        String[] typesArr = {"light", "temperature"};
+        for (String i : typesArr) {
+            types.put(i);
+        }
+        params.put("types", types);
+
+        query.put("method", "query_readings");
+        query.put("group_id", 22);
+        query.put("params", params);
+
+        try {
+            BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
+            PrintWriter out = new PrintWriter(s.getOutputStream(), true);
+
+            System.out.println("Querying server...");
+            out.write(query.toString());
+            out.println();
+            
+            System.out.println("Retrieving server reply...");
+            returned = new JSONObject(in.readLine());
+            System.out.println("Server reply received.");
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+        
+        DisplayQueryResults(returned);
+    }
+    
+    public static void GetDataSumm(Socket s) {
+        try {
+            BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
+            PrintWriter out = new PrintWriter(s.getOutputStream(), true);
+
+            JSONObject j = new JSONObject();
+            j.put("method", "data_summary");
+            j.put("group_id", "22");
+
+            out.write(j.toString());
+            out.println();
+
             JSONObject returned = new JSONObject(in.readLine()); //Object to store returning string from server
 
-            System.out.println("Reply from Server:" + returned.getString("result"));
-            
+            JSONArray dataArr = returned.getJSONArray("result"); //array of JSONobjects
+
+            System.out.println("============");
+
+            for (int i = 0; i < dataArr.length(); i++) {
+                JSONObject temp = new JSONObject();
+                temp = dataArr.getJSONObject(i);
+                System.out.println("Type: " + temp.get("type") + "\nType ID: " + temp.get("type_id") + "\nMean: " + temp.get("mean")
+                        + "\nMin: " + temp.get("min") + "\nMax: " + temp.get("max") + "\nStandard dev: " + temp.get("stddev") + "\n");
+                System.out.println("============");
+
+            }
+
+            //System.out.println("Reply from Server:" + returned.get("result") + " Time(ms):" + returned.getInt("elapsed"));
+
         } catch (IOException e) {
             System.out.println(e);
         }
@@ -145,37 +245,19 @@ public class ClientGroup22 {
         return sock;
     }
 
-    public static void GetDataSumm(Socket s) {
+    public static boolean isInteger(String s) {
         try {
-            BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
-            PrintWriter out = new PrintWriter(s.getOutputStream(), true);
-
-            JSONObject j = new JSONObject();
-            j.put("method", "data_summary");
-            j.put("group_id", "22");
-
-            out.write(j.toString());
-            out.println();
-
-            JSONObject returned = new JSONObject(in.readLine()); //Object to store returning string from server
-
-            JSONArray dataArr = returned.getJSONArray("result"); //array of JSONobjects
-
-            System.out.println("============");
-
-            for (int i = 0; i < dataArr.length(); i++) {
-                JSONObject temp = new JSONObject();
-                temp = dataArr.getJSONObject(i);
-                System.out.println("Type: " + temp.get("type") + "\nType ID: " + temp.get("type_id") + "\nMean: " + temp.get("mean")
-                        + "\nMin: " + temp.get("min") + "\nMax: " + temp.get("max") + "\nStandard dev: " + temp.get("stddev") + "\n");
-                System.out.println("============");
-
-            }
-
-            //System.out.println("Reply from Server:" + returned.get("result") + " Time(ms):" + returned.getInt("elapsed"));
-
-        } catch (IOException e) {
-            System.out.println(e);
+            Integer.parseInt(s);
+        } catch (NumberFormatException e) {
+            return false;
         }
+        // only got here if we didn't return false
+        return true;
     }
+    
+    public static void DisplayQueryResults(JSONObject j) {
+        System.out.println(j.toString());
+    }
+
+    
 }
