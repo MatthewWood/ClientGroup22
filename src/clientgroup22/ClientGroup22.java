b6cfed49d63d12d27006676e98075b569cb8e859
matthew <matthew@matthew-mint>
2013-04-09 13:51:05 +0200

    Added get data summary method - GetDataSumm(Socket s) - changed, tested, and working

--- src/clientgroup22/ClientGroup22.java
+++ src/clientgroup22/ClientGroup22.java
@@ -23,34 +23,34 @@ public class ClientGroup22 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-
+        
         Socket s = Connect();
         Ping(s);
         GetDataSumm(s);
-
+        
         ClientGroup22 client = new ClientGroup22();
-
+        
     }
-
+    
     public ClientGroup22() {
     }
-
+    
     public static void DisplayMenu() {
         System.out.println("Hello!");
     }
-
+    
     public static void Ping(Socket s) {
         try {
             BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
             PrintWriter out = new PrintWriter(s.getOutputStream(), true);
-
+            
             JSONObject j = new JSONObject();
             j.put("method", "ping");
             j.put("group_id", "22");
-
+            
             out.write(j.toString());
             out.println();
-
+            
             JSONObject returned = new JSONObject(in.readLine());   //Object to store returning string from server
 
             System.out.println("Reply from Server:" + returned.get("result") + " Time(ms):" + returned.getInt("elapsed"));
@@ -58,7 +58,7 @@ public class ClientGroup22 {
             System.out.println(e);
         }
     }
-
+    
     public static Socket Connect() {
         Socket sock = new Socket();
         try {
@@ -68,30 +68,32 @@ public class ClientGroup22 {
         }
         return sock;
     }
-
+    
     public static void GetDataSumm(Socket s) {
         try {
             BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
             PrintWriter out = new PrintWriter(s.getOutputStream(), true);
-
+            
             JSONObject j = new JSONObject();
             j.put("method", "data_summary");
             j.put("group_id", "22");
-
+            
             out.write(j.toString());
             out.println();
-
+            
             JSONObject returned = new JSONObject(in.readLine());   //Object to store returning string from server
 
-            JSONArray dataArr = returned.getJSONArray("return");   //array of JSONobjects
+            JSONArray dataArr = returned.getJSONArray("result");   //array of JSONobjects
+            
+            System.out.println("============");
 
             for (int i = 0; i < dataArr.length(); i++) {
                 JSONObject temp = new JSONObject();
                 temp = dataArr.getJSONObject(i);
-                System.out.println("Type: " + temp.get("type\n") + "Type ID: " + temp.get("type_id\n") + "Mean: " + temp.get("mean\n")
-                        + "Min: " + temp.get("min\n") + "Max: " + temp.get("max\n") + "Standard dev: " + temp.get("stdev\n"));
+                System.out.println("Type: " + temp.get("type") + "\nType ID: " + temp.get("type_id") + "\nMean: " + temp.get("mean")
+                        + "\nMin: " + temp.get("min") + "\nMax: " + temp.get("max") + "\nStandard dev: " + temp.get("stddev") + "\n");
                 System.out.println("============");
-
+                
             }
 
             //System.out.println("Reply from Server:" + returned.get("result") + " Time(ms):" + returned.getInt("elapsed"));
